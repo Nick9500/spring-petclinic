5d4f225afd44abd6895ca52dbecaf46e461bda1c
Earthii <xiao_eric@hotmail.com>
2018-04-01 14:49:36 -0400

    added ID to data to be forklifted in Mowner and also fixed lastname forklist

--- src/main/java/org/springframework/samples/petclinic/dataMigration/forklift/ForkLift.java
+++ src/main/java/org/springframework/samples/petclinic/dataMigration/forklift/ForkLift.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.dataMigration.forklift;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.dataMigration.mongoSampleIntegration.EmployeeRepository;
 import org.springframework.samples.petclinic.dataMigration.mowner.MOwner;
 import org.springframework.samples.petclinic.dataMigration.mowner.OwnerMRepository;
 import org.springframework.samples.petclinic.owner.Owner;
@@ -13,9 +12,6 @@ import java.util.Collection;
 @Component
 public class ForkLift {
     @Autowired
-    EmployeeRepository employeeRepository;
-
-    @Autowired
     OwnerMRepository ownerMRepository;
 
     @Autowired
@@ -23,26 +19,24 @@ public class ForkLift {
 
     public void start(){
         System.out.println("Start forklifting...");
-        employeeRepository.deleteAll();
+        ownerMRepository.deleteAll();
 
         Collection<Owner> forkliftData = ownerRepository.findAll();
         for (Owner owner : forkliftData){
-            //testing Saving objects into Employee table
-//            employeeRepository.save(new Employee(owner.getFirstName(), owner.getLastName()));
-//            System.out.println(employeeRepository.findByFirstName(owner.getFirstName()));
 
             //BROKEN AS FK
             MOwner mowner = new MOwner();
+            mowner.setId(owner.getId().toString());
             mowner.setFirstName(owner.getFirstName());
-            mowner.setFirstName(owner.getLastName());
+            mowner.setLastName(owner.getLastName());
             mowner.setAddress(owner.getAddress());
             mowner.setCity(owner.getCity());
             mowner.setTelephone(owner.getTelephone());
-
+            System.out.println(mowner);
             ownerMRepository.save(mowner);
 
         }
-        System.out.println("Done forklifting");
+        System.out.println("Done forklifting Owners");
 
     }
 }
