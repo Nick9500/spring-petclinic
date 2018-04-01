b884e7bfe9aea61a9ab04961bfef0a79bcaa7dbb
Earthii <xiao_eric@hotmail.com>
2018-04-01 18:04:26 -0400

    Revert " hotfix owner forklift because of missing pets attr"
    This reverts commit 3e10ade783f1160fb0490a5376c885c76e709019.

--- src/main/java/org/springframework/samples/petclinic/dataMigration/mowner/MOwner.java
+++ src/main/java/org/springframework/samples/petclinic/dataMigration/mowner/MOwner.java
@@ -11,12 +11,17 @@ import java.util.*;
 @Document(collection="owners")
 public class MOwner extends MPerson {
 
+//    @NotEmpty
     private String address;
 
+//    @NotEmpty
     private String city;
 
+//    @NotEmpty
+//    @Digits(fraction = 0, integer = 10)
     private String telephone;
 
+//    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<MPet> pets;
 
     public String getAddress() {
@@ -50,7 +55,7 @@ public class MOwner extends MPerson {
         return this.pets;
     }
 
-    public void setPets(Set<MPet> pets) {
+    protected void setPetsInternal(Set<MPet> pets) {
         this.pets = pets;
     }
 
